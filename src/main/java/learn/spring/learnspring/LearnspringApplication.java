527ed085313b0ae7ab6ecb968efb01979b9584b1
俞澄 <1099312164@qq.com>
2023-04-16 23:22:40 +0800

    first commit

---
+++ src/main/java/learn/spring/learnspring/LearnspringApplication.java
@@ -0,0 +1,22 @@
+package learn.spring.learnspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class LearnspringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnspringApplication.class, args);
+    }
+
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "ycc") String name) {
+        return "Hello " + name + " !";
+    }
+}
---
+++ src/test/java/learn/spring/learnspring/LearnspringApplicationTests.java
@@ -0,0 +1,13 @@
+package learn.spring.learnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
